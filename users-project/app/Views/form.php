e9cdd7ecaaf8714cc0e8a32555602522db72e825
a2va <al.adriana.v@gmail.com>
2021-04-12 21:50:50 -0300

    Alterações para input estados e cidades

--- users-project/app/Views/form.php
+++ users-project/app/Views/form.php
@@ -30,12 +30,16 @@
 
         <div class="form-group">
             <label for="user_state">Estado</label>
-            <input type="text" value="<?= isset($user['user_state']) ? $user['user_state'] : ''; ?>" name="user_state" id="user_state" class="form-control" placeholder="Selecione o estado">
+            <select name="user_state" id="user_state" class="form-select"> 
+                <option disabled>Selecione estado</option>
+            </select>            
         </div>
 
         <div class="form-group">
-            <label for="user_city">Estado</label>
-            <input type="text" value="<?= isset($user['user_city']) ? $user['user_city'] : ''; ?>" name="user_city" id="user_city" class="form-control" placeholder="cidade...">
+            <label for="user_city">Cidade</label>
+            <select name="user_city" id="user_city" class="form-select">
+                <option disabled>Selecione cidade</option>
+            </select>            
         </div>
 
         <div class="form-group">
@@ -48,8 +52,58 @@
         <div class="form-group">
             <input type="hidden" value="<?= isset($user['id']) ? $user['id'] : ''; ?>" name="id" id="id">
             <input type="submit" value="Salvar" class="btn btn-primary">
+            <?= anchor(base_url(), 'Retornar', ['class' => 'btn btn-secondary float-end'])?>
         </div>
     <?= form_close() ?> 
     <br>   
 </div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script type="text/javascript">	
+		
+    $(document).ready(function () {
+        
+        var user_state = '<?php echo isset($user['user_state']) ? $user['user_state'] : ''; ?>';        
+        var user_city = '<?php echo isset($user['user_city']) ? $user['user_city'] : ''; ?>';        
+        var states = '<?php base_url() ?>/assets/js/EstateAndCity.json';
+        
+        $.getJSON(states, function (data) {
+
+            //var items = [];
+            var options = '<option value="">escolha um estado</option>';	
+            
+            $.each(data, function (key, val) {
+                if(user_state == val.nome){
+                    options += '<option selected value="' + val.nome + '">' + val.nome + '</option>';
+                }else{
+                    options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+                }			
+            });		
+            
+            $("#user_state").html(options);				
+            
+            $("#user_state").change(function () {
+                var options_city = '';
+                var str = "";					
+                
+                $("#user_state option:selected").each(function () {
+                    str += $(this).text();
+                });
+                
+                $.each(data, function (key, val) {
+                    if(val.nome == str) {							
+                        $.each(val.cidades, function (key_city, val_city) {
+                            if(val_city == user_city){
+                                options_city += '<option selected value="' + val_city + '">' + val_city + '</option>';
+                            }else{
+                                options_city += '<option value="' + val_city + '">' + val_city + '</option>';
+                            }
+                        });							
+                    }
+                });
+                $("#user_city").html(options_city);
+            }).change();
+        });    
+    });
+    
+</script>		
 <?php echo $this->endSection(); ?>
